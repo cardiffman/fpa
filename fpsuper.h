15370186f06fedf14d33572066524386048863d7
cardiffman <cardiffman@yahoo.com>
2018-05-15 05:05:38 -0700

    [FPA] better fpsuper version

--- fpsuper.h
+++ fpsuper.h
@@ -1,29 +1,26 @@
 /*
  * fpsuper.h
  *
- *  Created on: May 10, 2018
- *      Author: Mike
+ *  Created on: Mar 27, 2018
+ *      Author: menright
  */
 
 #ifndef FPSUPER_H_
 #define FPSUPER_H_
-
 #include <list>
-#include <memory>
-#include <string>
+
 #include "fpnode.h"
-struct Token;
-struct Node;
 
 struct CoreScDefn {
 	std::string name;
 	std::list<std::string> args;
 	std::shared_ptr<Node> expr;
+	std::string to_string() const;
 };
-
+struct Token;
 std::list<CoreScDefn> Definitions(Token& token);
 
-//CoreScDefn
-//Definition(const std::string& name, const std::shared_ptr<Node>& expr);
+
+
 
 #endif /* FPSUPER_H_ */
